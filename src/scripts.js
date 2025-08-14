b208550ebf2e83cf3aa56fbd2033e6b9eadd567a
Greyson Peck <greyson.peck@gmail.com>
2025-08-13 17:01:49 -0700

    remove ads and hide consent ask temporarily

--- src/scripts.js
+++ src/scripts.js
@@ -801,31 +801,31 @@ function makeSlot(id, label, hasFoil, quantity) {
     }
 
     // Put an ad
-    if (cardSection.childElementCount === 4) {
-        // Create the container <ins>
-        var adMid = document.createElement("ins");
-        adMid.className = "adsbygoogle";
-        adMid.style = "display:inline-block;width:970px;height:90px";
-        adMid.setAttribute("data-ad-client", "ca-pub-1084747507972985"); // your AdSense publisher ID
-        adMid.setAttribute("data-ad-slot", "3445711329"); // your Ad unit slot ID
-        // adMid.setAttribute("data-ad-format", "auto");
-        // adMid.setAttribute("data-full-width-responsive", "true");
+    // if (cardSection.childElementCount === 4) {
+    //     // Create the container <ins>
+    //     var adMid = document.createElement("ins");
+    //     adMid.className = "adsbygoogle";
+    //     adMid.style = "display:inline-block;width:970px;height:90px";
+    //     adMid.setAttribute("data-ad-client", "ca-pub-1084747507972985"); // your AdSense publisher ID
+    //     adMid.setAttribute("data-ad-slot", "1234567890"); // your Ad unit slot ID
+    //     // adMid.setAttribute("data-ad-format", "auto");
+    //     // adMid.setAttribute("data-full-width-responsive", "true");
 
-        // Create Ad Holder
-        adHolder = document.createElement("div");
-        adHolder.id = "ad-holder-middle";
-        adHolder.classList.add("flex", "justify-center", "w-full", "min-h-[90px]");
-        cardSection.appendChild(adHolder);
+    //     // Create Ad Holder
+    //     adHolder = document.createElement("div");
+    //     adHolder.id = "ad-holder-middle";
+    //     adHolder.classList.add("ad-holder", "flex", "justify-center", "w-full", "min-h-[90px]");
+    //     cardSection.appendChild(adHolder);
 
-        // Insert the ad into the DOM
-        adHolder.insertAdjacentElement("beforeend", adMid);
+    //     // Insert the ad into the DOM
+    //     adHolder.insertAdjacentElement("beforeend", adMid);
 
-        // Tell AdSense to render the ad
-        // Delay rendering to ensure width is calculated
-        setTimeout(() => {
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        }, 50);
-    }
+    //     // Tell AdSense to render the ad
+    //     // Delay rendering to ensure width is calculated
+    //     setTimeout(() => {
+    //         (adsbygoogle = window.adsbygoogle || []).push({});
+    //     }, 50);
+    // }
 }
 
 function setGhostData() {
